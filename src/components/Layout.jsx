42020e42ca17348c36fba01456870ababaf2e291
bruce <bmpandrada@gmail.com>
2025-09-22 16:16:51 +0800

    fix min height

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -6,15 +6,17 @@ import Nav from "./Nav";
 const Layout = ({ children,  }) => {
   const {error, loading, sortBy, setSortby, filter, setFilter} = useContext(ContextApi);
     return (
-      <div className="w-full">
+      <div className="flex flex-col min-h-screen">
           <h1 className="text-3xl font-bold text-center mb-8 text-slate-400 bg-black p-5">
           🎌 Top Anime Pillow
         </h1>
-          <Nav sortBy={sortBy} setSortby={setSortby} filter={filter} setFilter={setFilter} />
+         <div className="flex-1">
+           <Nav sortBy={sortBy} setSortby={setSortby} filter={filter} setFilter={setFilter} />
 
         {loading && <p>Loading...</p>}
         {error && <p>Error Message: {error}</p>}
       {children}
+         </div>
       <div className="bg-black text-center p-5 text-white">
           <p className="text-sm">Created By: BMPA {new Date().getFullYear()}</p>
       </div>
