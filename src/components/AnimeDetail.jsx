7bbdd3ef54cd254fcf2ebfd954ee50cf193a4075
bruce <bmpandrada@gmail.com>
2025-10-29 16:46:02 +0800

    fix: update data destructuring

--- src/components/AnimeDetail.jsx
+++ src/components/AnimeDetail.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router";
 import SpinnerLoading from "./SpinnerLoader";
 
 export default function AnimeDetail() {
-  const { id } =  useParams();
+  const { id } = useParams();
   const [anime, setAnime] = useState(null);
 
   useEffect(() => {
@@ -18,26 +18,22 @@ export default function AnimeDetail() {
     fetchAnime();
   }, [id]);
 
-   if (!anime)
-    return (
-      <SpinnerLoading />
-    );
-
+  if (!anime) return <SpinnerLoading />;
 
   return (
-      <div className="max-w-3xl mx-auto bg-white rounded-2xl shadow p-10 mb-10">
-        <img
-          src={anime.images.jpg.large_image_url}
-          alt={anime.title}
-          className="w-full max-w-xs sm:max-w-md mx-auto rounded-xl mb-4 sm:object-contain"
-        />
-        <h1 className="text-3xl font-bold mb-2">{anime.title}</h1>
-        <p className="text-gray-600 mb-4">{anime.synopsis}</p>
-        <div className="flex gap-4 text-sm text-gray-700">
-          <span>⭐ {anime.score.toFixed(2) || "N/A"}</span>
-          <span>🎬 {anime.episodes || "?"} episodes</span>
-          <span>📅 {anime.year || "Unknown"}</span>
-        </div>
+    <div className='max-w-3xl mx-auto bg-white rounded-2xl shadow p-10 mb-10'>
+      <img
+        src={anime.images.webp.large_image_url}
+        alt={anime.title}
+        className='w-full max-w-xs sm:max-w-md mx-auto rounded-xl mb-4 sm:object-contain'
+      />
+      <h1 className='text-3xl font-bold mb-2'>{anime.title}</h1>
+      <p className='text-gray-600 mb-4'>{anime.synopsis}</p>
+      <div className='flex gap-4 text-sm text-gray-700'>
+        <span>⭐ {anime.score.toFixed(2) || "N/A"}</span>
+        <span>🎬 {anime.episodes || "?"} episodes</span>
+        <span>📅 {anime.year || "Unknown"}</span>
       </div>
+    </div>
   );
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,72 +4,72 @@ import { useAnime } from "../context/ContextApi";
 import { Link } from "react-router";
 
 const HomePage = () => {
-    const {filteredAnime, loading} = useAnime();
-    const [currentPage, setCurrentPage] = useState(1);
-    const perPage = 12;
+  const { filteredAnime, loading } = useAnime();
+  const [currentPage, setCurrentPage] = useState(1);
+  const perPage = 12;
 
-    const totalPage = Math.ceil(filteredAnime.length / perPage);
+  const totalPage = Math.ceil(filteredAnime.length / perPage);
 
-    const lastIndex = currentPage * perPage;
-    const firstIndex = lastIndex - perPage;
-    const currentAnime = filteredAnime.slice(firstIndex, lastIndex);  
+  const lastIndex = currentPage * perPage;
+  const firstIndex = lastIndex - perPage;
+  const currentAnime = filteredAnime.slice(firstIndex, lastIndex);
 
+  if (loading) return <SpinnerLoading />;
 
-    if (loading)
-    return (
-      <SpinnerLoading />
-    );
-
-
-    return ( 
-        <div className="px-5 sm:px-10">  
-
-         <div className="grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-5">
-        {filteredAnime.length === 0 ?
-        <div className="flex justify-center items-center min-h-[60vh] col-span-full">
-          <p className="text-center font-bold text-4xl text-gray-600">
-            Not Found
-          </p>
-        </div>
-        : currentAnime.map((item) => (
-          <Link  to={`/anime/${item.mal_id}`}
-            key={item.mal_id}
-            className="bg-white rounded-2xl shadow hover:shadow-lg transition overflow-hidden"
+  return (
+    <div className='px-5 sm:px-10'>
+      <div className='grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-5'>
+        {filteredAnime.length === 0 ? (
+          <div className='flex justify-center items-center min-h-[60vh] col-span-full'>
+            <p className='text-center font-bold text-4xl text-gray-600'>
+              Not Found
+            </p>
+          </div>
+        ) : (
+          currentAnime.map((item) => (
+            <Link
+              to={`/anime/${item.mal_id}`}
+              key={item.mal_id}
+              className='bg-white rounded-2xl shadow hover:shadow-lg transition overflow-hidden'
+            >
+              <img
+                src={item.images.webp.large_image_url}
+                alt={item.title}
+                className='w-full h-60 object-cover transform transition-transform duration-300 hover:scale-105'
+              />
+              <div className='p-4'>
+                <h2 className='text-lg font-semibold text-gray-900 truncate'>
+                  {item.title}
+                </h2>
+                <p className='text-sm text-gray-600 mt-1'>
+                  Episodes: {item.episodes || "?"}
+                </p>
+                <p className='text-yellow-500 font-bold mt-2'>
+                  ⭐ {item.score.toFixed(1) || "N/A"}
+                </p>
+              </div>
+            </Link>
+          ))
+        )}
+      </div>
+      <div className='flex justify-center gap-2 mt-8'>
+        {Array.from({ length: totalPage }, (_, ibtn) => (
+          <button
+            key={ibtn + 1}
+            onClick={() => setCurrentPage(ibtn + 1)}
+            className={`px-3 py-1  cursor-pointer rounded mb-10 
+                  ${
+                    currentPage === ibtn + 1
+                      ? "bg-slate-600 text-white"
+                      : "bg-slate-700 text-gray-200"
+                  }`}
           >
-            <img
-              src={item.images.jpg.image_url}
-              alt={item.title}
-              className="w-full h-60 object-cover transform transition-transform duration-300 hover:scale-105"
-            />
-            <div className="p-4">
-              <h2 className="text-lg font-semibold text-gray-900 truncate">
-                {item.title}
-              </h2>
-              <p className="text-sm text-gray-600 mt-1">
-                Episodes: {item.episodes || "?"}
-              </p>
-              <p className="text-yellow-500 font-bold mt-2">
-                ⭐ {item.score.toFixed(1) || "N/A"}
-              </p>
-            </div>
-          </Link>
+            {ibtn + 1}
+          </button>
         ))}
       </div>
-        <div className="flex justify-center gap-2 mt-8">
-            {Array.from({length: totalPage}, (_, ibtn)=>(
-              <button 
-                  key={ibtn + 1} 
-                  onClick={()=> setCurrentPage(ibtn + 1)}
-                  className={`px-3 py-1  cursor-pointer rounded mb-10 
-                  ${currentPage === ibtn + 1 ? 'bg-slate-600 text-white' : 'bg-slate-700 text-gray-200'}`}>
-                      {ibtn + 1}
-                </button>
-              ))}
-        </div>
-        </div>
-     );
-}
- 
+    </div>
+  );
+};
+
 export default HomePage;
-
-
