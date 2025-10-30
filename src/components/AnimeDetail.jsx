11abb126e688afffa84929e7151d73bb8d3db59e
bruce <bmpandrada@gmail.com>
2025-10-30 15:22:06 +0800

    fix: add trailer video

--- src/components/AnimeDetail.jsx
+++ src/components/AnimeDetail.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import SpinnerLoading from "./SpinnerLoader";
+import VideoPlayer from "./VideoPlayer";
+import YouTubePlayer from "./VideoPlayer";
+import TrailerPlayer from "./VideoPlayer";
 
 export default function AnimeDetail() {
   const { id } = useParams();
@@ -22,17 +25,22 @@ export default function AnimeDetail() {
 
   return (
     <div className='max-w-3xl mx-auto bg-white rounded-2xl shadow p-10 mb-10'>
-      <img
-        src={anime.images.webp.large_image_url}
-        alt={anime.title}
-        className='w-full max-w-xs sm:max-w-md mx-auto rounded-xl mb-4 sm:object-contain'
-      />
-      <h1 className='text-3xl font-bold mb-2'>{anime.title}</h1>
-      <p className='text-gray-600 mb-4'>{anime.synopsis}</p>
-      <div className='flex gap-4 text-sm text-gray-700'>
+      <TrailerPlayer trailer={anime.trailer.embed_url} />
+      <div className='flex gap-4 text-sm text-gray-700 mb-5'>
         <span>⭐ {anime.score.toFixed(2) || "N/A"}</span>
         <span>🎬 {anime.episodes || "?"} episodes</span>
         <span>📅 {anime.year || "Unknown"}</span>
+      </div>
+      <div className='grid sm:grid-cols-4 items-center gap-2'>
+        <div className='sm:col-span-3'>
+          <h1 className='text-3xl font-bold mb-2'>{anime.title}</h1>
+          <p className='text-gray-600 mb-4'>{anime.synopsis}</p>
+        </div>
+        <img
+          src={anime.images.webp.large_image_url}
+          alt={anime.title}
+          className='w-full max-w-sm mx-auto h-50 sm:h-auto rounded-xl mb-4 object-contain'
+        />
       </div>
     </div>
   );
---
+++ src/components/VideoPlayer.jsx
@@ -0,0 +1,27 @@
+export default function TrailerPlayer({ trailer }) {
+  const embedUrl = `${trailer}=1&mute=1`;
+
+  return (
+    <div
+      style={{
+        position: "relative",
+        paddingTop: "56.25%",
+      }}
+    >
+      <iframe
+        src={embedUrl}
+        title='Trailer'
+        allow='autoplay; encrypted-media; picture-in-picture'
+        allowFullScreen
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          border: "none",
+        }}
+      />
+    </div>
+  );
+}
