169fbf7f4918a2c75de56688265c0dcaad24b817
bruce <bmpandrada@gmail.com>
2025-11-05 14:35:01 +0800

    fix: add spacing card character mobile view

--- src/components/CharacterCards.jsx
+++ src/components/CharacterCards.jsx
@@ -5,7 +5,7 @@ const CharacterCards = ({ char = [] }) => {
       <h1 className='mt-10 text-lg sm:text-2xl font-semibold text-success'>
         Characters
       </h1>
-      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6 mt-5 mb-10'>
+      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 sm:gap-6 mt-5 mb-10'>
         {char.map((char) => (
           <div className='card bg-base-200 w-full shadow-sm'>
             <figure>
