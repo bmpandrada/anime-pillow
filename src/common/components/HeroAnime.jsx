bacdb05ab44faf42b39a7230d0b448e0054b8e54
bruce <bmpandrada@gmail.com>
2025-11-06 20:16:44 +0800

    fix: update social links

--- src/common/components/HeroAnime.jsx
+++ src/common/components/HeroAnime.jsx
@@ -61,10 +61,13 @@ const HeroAnime = ({ displayedAnime }) => {
             )}
             <br /> <br />
             <div className='flex items-center gap-2 text-warning'>
-              <a href={`${import.meta.env.VITE_LINK_LINKEDIN}`} target='_blank'>
+              <a
+                href={`https://www.linkedin.com/in/bruce-michael-andrada-565b561a4/`}
+                target='_blank'
+              >
                 <FaLinkedin className='text-2xl hover:text-blue-400 transition' />
               </a>
-              <a href={`${import.meta.env.VITE_LINK_GITHUB}`} target='_blank'>
+              <a href={`https://github.com/bmpandrada`} target='_blank'>
                 <FaGithub className='text-2xl hover:text-blue-400 transition' />
               </a>
             </div>
