b0211f6bcedb4e30ddda9c0ce36b616cc1dfc2dd
bruce <bmpandrada@gmail.com>
2025-11-07 12:31:25 +0800

    fix: add cookie and refactor context

--- src/common/components/Toogle.jsx
+++ src/common/components/Toogle.jsx
@@ -1,28 +1,9 @@
-// export default ThemeToggle;
-import { useEffect, useState } from "react";
 import { WiDaySunny } from "react-icons/wi";
 import { AiOutlineMoon } from "react-icons/ai";
+import { useAnime } from "../context/ContextApi";
 
 const ThemeToggle = () => {
-  const getInitialTheme = () => {
-    const savedTheme = localStorage.getItem("theme");
-    if (savedTheme) return savedTheme;
-
-    // Detect time: 6PM - 6AM = dark mode
-    const hour = new Date().getHours();
-    return hour >= 18 || hour < 6 ? "mytheme" : "mytheme-light";
-  };
-
-  const [theme, setTheme] = useState(getInitialTheme);
-
-  useEffect(() => {
-    const html = document.querySelector("html");
-    if (html) {
-      html.setAttribute("data-theme", theme);
-    }
-    localStorage.setItem("theme", theme);
-  }, [theme]);
-
+  const { theme, setTheme } = useAnime();
   const toggleTheme = () => {
     setTheme((prev) => (prev === "mytheme" ? "mytheme-light" : "mytheme"));
   };
