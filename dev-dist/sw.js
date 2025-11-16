b770007334cdc5ec22e649723577dfc91721570d
bruce <bmpandrada@gmail.com>
2025-11-16 09:59:24 +0800

    fix: add transition smooth css for toggle theme

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ilc9dk6ba98"
+    "revision": "0.o7t11di4h9"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
