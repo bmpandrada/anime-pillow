39569bd341285801ed9ef5d90848ae948cc76cb0
bruce <bmpandrada@gmail.com>
2025-11-17 18:21:08 +0800

    fix: update html pub/theme/meta

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-47da91e0'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.986r4v5nvp"
+    "revision": "0.9ktspnupak"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
